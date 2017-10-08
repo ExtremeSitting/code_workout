f1ccd813107d629ecb785d18eeb5b16160bc8c68
Sam Chapler <sam@txlans.com>
2017-10-08 11:52:47 -0500

    random dates. Because you should have to work for it.

--- 1/sample_output.sh
+++ 1/sample_output.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-for i in {1..100}; do echo $i "ID:"$(uuidgen) "power: "$(shuf -i 1-100 -n 1) "access: "$(date -uR); done | column -t
+for i in {1..100}; do echo $i "ID:"$(uuidgen) "power: "$(shuf -i 1-100 -n 1) "access: "$(date -uRd @$(shuf -i 0-$(date +'%s') -n 1)); done | column -t
